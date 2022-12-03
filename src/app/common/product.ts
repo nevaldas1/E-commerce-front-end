d077fdb7b2dd834a665222fdbf37d683032e88fd
nevaldas1 <97054786+nevaldas1@users.noreply.github.com>
2022-12-03 16:46:03 +0200

    Create product-details component and route

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -1,6 +1,7 @@
 export class Product {
 
-    constructor(public sku: string,
+    constructor(public id: number,
+                public sku: string,
                 public name: string,
                 public description: string,
                 public unitPrice: number,
@@ -8,6 +9,7 @@ export class Product {
                 public active: boolean,
                 public unitsInStock: number,
                 public dateCreated: Date,
-                public dateUpdated: Date) {}
+                public dateUpdated: Date) 
+                {}
             
 }
