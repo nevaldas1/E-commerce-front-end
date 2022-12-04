d98f9cee5cb52526f6adeedc58f3e98e793d99f4
nevaldas1 <97054786+nevaldas1@users.noreply.github.com>
2022-12-04 14:20:56 +0200

    Define CartItem class and create constructor

--- src/app/common/cart-item.ts
+++ src/app/common/cart-item.ts
@@ -1,2 +1,18 @@
+import { Product } from "./product";
+
 export class CartItem {
+
+    id: number;
+    name: string;
+    imageurl: string;
+    unitPrice: number;
+    quantity: number;
+
+    constructor(product: Product) {
+        this.id = product.id;
+        this.name = product.name;
+        this.imageurl = product.imageUrl;
+        this.unitPrice = product.unitPrice;
+        this.quantity = 1;
+    }
 }
