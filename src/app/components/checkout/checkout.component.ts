ada08a652c323e9296097655abfa7d24561a3286
nevaldas1 <97054786+nevaldas1@users.noreply.github.com>
2022-12-04 23:24:44 +0200

    Add "review your order" section in checkout form

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -10,6 +10,9 @@ export class CheckoutComponent implements OnInit{
   
   checkoutFormGroup: FormGroup;
 
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
@@ -33,7 +36,7 @@ export class CheckoutComponent implements OnInit{
         country: [''],
         zipCode: ['']
       }),
-      creditCardInformation: this.formBuilder.group({
+      creditCard: this.formBuilder.group({
         cardType: [''],
         nameOnCard: [''],
         cardNumber: [''],
@@ -41,7 +44,7 @@ export class CheckoutComponent implements OnInit{
         expirationMonth: [''],
         expirationYear: ['']
       }),
-    })
+    });
   }
 
 
