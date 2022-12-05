37a873c471e02b64acba720eafbe035a297b1c93
nevaldas1 <97054786+nevaldas1@users.noreply.github.com>
2022-12-05 17:52:27 +0200

    Add service methods to get Country and State

--- src/app/services/checkout-form.service.ts
+++ src/app/services/checkout-form.service.ts
@@ -1,12 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Country } from '../common/country';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutFormService {
 
-  constructor() { }
+  private countriesUrl = "http://localhost:8080/api/countries";
+  private statesUrl = "http://localhost:8080/api/states"
+
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  getCountries(): Observable<Country[]> {
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries)
+    );
+  }
+
+
+  getStates(theCountryCode: string): Observable<State[]> {
+
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${theCountryCode}`;
+
+    return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+      map(response => response._embedded.states)
+    );
+  }
 
 
   getCreditCardYears(): Observable<number[]> {
@@ -34,4 +59,17 @@ export class CheckoutFormService {
     return of(data);
   }
 
+}
+
+
+interface GetResponseCountries {
+  _embedded: {
+    countries: Country[];
+  }
+}
+
+interface GetResponseStates {
+  _embedded: {
+    states: State[];
+  }
 }
