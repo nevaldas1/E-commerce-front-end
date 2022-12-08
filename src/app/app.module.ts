06e37b8d2e6783fc85cd0dd4d165e190b1280f04
nevaldas1 <97054786+nevaldas1@users.noreply.github.com>
2022-12-08 17:16:52 +0200

    Add OrderHistory button and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ function sendToLoginPage(oktaAuth: OktaAuth, injector: Injector) {
 }
 
 const routes: Routes = [
+  {path: 'order-history', component: OrderHistoryComponent, canActivate: [OktaAuthGuard], data: {onAuthRequired: sendToLoginPage}},
   {path: 'members', component: MembersPageComponent, canActivate: [OktaAuthGuard], data: {onAuthRequired: sendToLoginPage}},
   {path: 'login/callback', component: OktaCallbackComponent},
   {path: 'login', component: LoginComponent},
